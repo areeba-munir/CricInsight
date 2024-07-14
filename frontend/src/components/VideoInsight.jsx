f87a644a7c36070c9c102f63ce4a9095c637edeb
Daoud-Hussain <daoudhussain302@gmail.com>
2024-07-14 15:16:50 +0500

    Managing states ft. Play/Forward/Rewind

--- frontend/src/components/VideoInsight.jsx
+++ frontend/src/components/VideoInsight.jsx
@@ -1,19 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button, Box, IconButton } from "@mui/material";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 import FastRewindIcon from "@mui/icons-material/FastRewind";
 import FastForwardIcon from "@mui/icons-material/FastForward";
 import FullscreenIcon from "@mui/icons-material/Fullscreen";
 
 const VideoInsight = () => {
   const [videoSrc, setVideoSrc] = useState("");
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
 
   const handleVideoUpload = (event) => {
     const file = event.target.files[0];
     if (file) {
       const url = URL.createObjectURL(file);
       setVideoSrc(url);
+      setIsPlaying(false);
+    }
+  };
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+        setIsPlaying(true);
+      } else {
+        videoRef.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  };
+
+  const handleRewind = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime -= 10;
+    }
+  };
+
+  const handleFastForward = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime += 10;
+    }
+  };
+
+  const handleFullscreen = () => {
+    if (videoRef.current) {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if (videoRef.current.mozRequestFullScreen) {
+        // Firefox
+        videoRef.current.mozRequestFullScreen();
+      } else if (videoRef.current.webkitRequestFullscreen) {
+        // Chrome, Safari, and Opera
+        videoRef.current.webkitRequestFullscreen();
+      } else if (videoRef.current.msRequestFullscreen) {
+        // IE/Edge
+        videoRef.current.msRequestFullscreen();
+      }
     }
   };
 
@@ -47,6 +92,7 @@ const VideoInsight = () => {
       </Box>
       <Box
         component="video"
+        ref={videoRef}
         width="80%"
         height="auto"
         bgcolor="#d3d3d3"
@@ -54,16 +100,16 @@ const VideoInsight = () => {
         src={videoSrc}
       />
       <Box display="flex" justifyContent="center" alignItems="center" mt={2}>
-        <IconButton>
+        <IconButton onClick={handleRewind}>
           <FastRewindIcon />
         </IconButton>
-        <IconButton>
-          <PlayArrowIcon />
+        <IconButton onClick={handlePlayPause}>
+          {isPlaying ? <PauseIcon /> : <PlayArrowIcon />}
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleFastForward}>
           <FastForwardIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleFullscreen}>
           <FullscreenIcon />
         </IconButton>
       </Box>
