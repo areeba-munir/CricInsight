b2c4215e062652d953c77097207cbf24a2530750
Daoud-Hussain <daoudhussain302@gmail.com>
2024-07-23 11:13:16 +0500

    Add payment gateway

---
+++ frontend/src/components/PaymentGateway.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+function PaymentGateway() {
+  return (
+    <Box>
+      <Typography variant="h4">Payment Gateway</Typography>
+      <Typography variant="body1">
+        This is the Payment Gateway component.
+      </Typography>
+    </Box>
+  );
+}
+
+export default PaymentGateway;
