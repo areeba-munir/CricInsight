d72b2174087dc1c7a77323f6a276d19e3741f907
Daoud-Hussain <daoudhussain302@gmail.com>
2024-09-27 02:48:49 +0500

    Login with google

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import './index.css';
-import App from './components/App';
-import { Provider } from 'react-redux';
-import store from './components/store';
+import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./components/store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <GoogleOAuthProvider clientId="84137165849-n5rpca9u1cfmerfb7um368peoc94doq5.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+    ;
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
